eeed913f6cdfb4b260c8d13a80636a9e0a7766e3
Playpaper <heartcolorful@gmail.com>
2023-03-26 12:15:32 +0800

    feat: require mongoose and database connection

--- app.js
+++ app.js
@@ -2,9 +2,24 @@
 const express = require('express')
 // require handlebars from express-handlebars
 const exphbs = require('express-handlebars')
+// require mongoose
+const mongoose = require('mongoose')
 
 const app = express()
 const port = 3000
+
+// database connection
+if(process.env.NODE_ENV !== 'production'){
+  require('dotenv').config()
+}
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+const db = mongoose.connection
+
+db.on('error', () => console.log('mongodb error !'))
+db.once('open', () => console.log('mongodb connected !'))
 
 // set template engine
 app.engine('hbs', exphbs({
