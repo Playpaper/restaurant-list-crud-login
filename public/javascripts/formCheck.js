9d16d19dba61749a389be5e09c7f0e0493c29a37
Playpaper <heartcolorful@gmail.com>
2023-04-25 22:14:50 +0800

    feat: add user registeration feature and form check

--- public/javascripts/formCheck.js
+++ public/javascripts/formCheck.js
@@ -1,10 +1,16 @@
 // Validation when submit form ( new / edit )
 const submitButton = document.querySelector('#btn-submit')
-const form = document.querySelector('#form')
+const form = document.querySelector('form')
 
-submitButton.addEventListener('click', function onSubmitButtonClicked(e) {
+form.addEventListener('submit', function onSubmitButtonClicked(e) {
+  if(!form.checkValidity()) {
+    e.stopPropagation()
+    e.preventDefault()
+  }
   form.classList.add('was-validated')
 })
+
+
 
 // const submitButton = document.querySelector('#submit-button')
 // submitButton.addEventListener('click', function onSubmitButtonClicked(event) {
