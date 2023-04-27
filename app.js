c2f57958563c352aa798cf6677d7055dec2438a7
Playpaper <heartcolorful@gmail.com>
2023-04-27 10:14:30 +0800

    feat: set passport and add user authentication

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require("method-override")
 const session = require('express-session')
+const usePassport = require('./config/passport')
 const helpers = require("./public/javascripts/helpers");
 const routes = require('./routes')
 const bodyParser = require('body-parser')
@@ -17,12 +18,14 @@ app.engine('hbs', exphbs({
   extname: '.hbs'
 }))
 app.set('view engine', 'hbs')
-
+// 
 app.use(session({
   secret: 'ThisIsMySecret',
   resave: false,
   saveUninitialized: true
 }))
+
+usePassport(app)
 
 app.use(express.static('public'))
 app.use(methodOverride('_method'))
